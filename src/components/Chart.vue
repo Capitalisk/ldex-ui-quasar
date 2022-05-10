e3e4a2db68a8e31ab7663979ae70194e5e3528af
Maarten Coppens <martinuske@gmail.com>
2022-05-09 21:51:57 -0300

    fix chart resize

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,9 +1,9 @@
 <template>
-  <div ref="ldexChartRef"></div>
+  <div ref="ldexChartRef" style="width: 100%"></div>
 </template>
 
 <script setup>
-import { onMounted, ref, computed, watchEffect } from 'vue';
+import { ref, computed, watchEffect, onMounted, onUnmounted } from 'vue';
 
 import { useStore } from 'src/store';
 import { useRoute } from 'vue-router';
@@ -25,83 +25,92 @@ const timeMultiplier = Number(marketConfig.value.timeMultiplier) || 1;
 const timeOffset = Number(marketConfig.value.timeOffset) || 0;
 
 const calculateChart = () => {
-  const volumeDisplayHeightRatio = 100000;
+  try {
+    const volumeDisplayHeightRatio = 100000;
 
-  const maxVolume = props.chartData.reduce(
-    (accumulator, entry) =>
-      Number(entry.volume) > accumulator ? Number(entry.volume) : accumulator,
-    -Infinity,
-  );
-  const maxPrice = props.chartData.reduce(
-    (accumulator, entry) =>
-      Number(entry.price) > accumulator ? Number(entry.price) : accumulator,
-    -Infinity,
-  );
+    const maxVolume = props.chartData.reduce(
+      (accumulator, entry) =>
+        Number(entry.volume) > accumulator ? Number(entry.volume) : accumulator,
+      -Infinity,
+    );
+    const maxPrice = props.chartData.reduce(
+      (accumulator, entry) =>
+        Number(entry.price) > accumulator ? Number(entry.price) : accumulator,
+      -Infinity,
+    );
 
-  const chartData = props.chartData.map((entry) => [
-    new Date(
-      Math.round(entry.baseTimestamp * timeMultiplier + timeOffset),
-    ).toLocaleDateString('en-GB', {
-      day: 'numeric',
-      month: 'short',
-      year: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-    }),
-    (Math.round((Number(entry.volume) * 100) / maxVolume) *
-      maxPrice *
-      volumeDisplayHeightRatio) /
-      unitValue,
-    entry.price,
-  ]);
-
-  google.charts.load('current', { packages: ['corechart'] });
-  google.charts.setOnLoadCallback(drawVisualization);
-
-  function drawVisualization() {
-    // Some raw data (not necessarily accurate)
-    const data = google.visualization.arrayToDataTable([
-      ['Time', 'Volume', 'Price'],
-      ...chartData.reverse(),
+    const chartData = props.chartData.map((entry) => [
+      new Date(
+        Math.round(entry.baseTimestamp * timeMultiplier + timeOffset),
+      ).toLocaleDateString('en-GB', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      }),
+      (Math.round((Number(entry.volume) * 100) / maxVolume) *
+        maxPrice *
+        volumeDisplayHeightRatio) /
+        unitValue,
+      entry.price,
     ]);
 
-    const options = {
-      // title: 'Monthly Coffee Production by Country',
-      seriesType: 'bars',
-      series: { 1: { type: 'line' } },
-      legend: 'none',
-      height: 600,
-      backgroundColor: {
-        fillOpacity: 0,
-      },
-      hAxis: {
-        textStyle: { color: '#FFF' },
-        gridlines: {
-          color: '#000',
-        },
-      },
-      vAxis: {
-        textStyle: { color: '#FFF' },
-        gridlines: {
-          interval: 0,
-        },
-      },
-      chartArea: {
-        left: 5,
-        top: 20,
-        width: '100%',
-        height: '350',
-      },
-      bar: { groupWidth: '100%' },
-      colors: ['#0366d6', '#FFF'],
-    };
+    google.charts.load('current', { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(drawVisualization);
 
-    const chart = new google.visualization.ComboChart(ldexChartRef.value);
-    chart.draw(data, options);
-  }
+    function drawVisualization() {
+      // Some raw data (not necessarily accurate)
+      const data = google.visualization.arrayToDataTable([
+        ['Time', 'Volume', 'Price'],
+        ...chartData.reverse(),
+      ]);
+
+      const options = {
+        // title: 'Monthly Coffee Production by Country',
+        seriesType: 'bars',
+        series: { 1: { type: 'line' } },
+        legend: 'none',
+        height: 600,
+        backgroundColor: {
+          fillOpacity: 0,
+        },
+        hAxis: {
+          textStyle: { color: '#FFF' },
+          gridlines: {
+            color: '#000',
+          },
+        },
+        vAxis: {
+          textStyle: { color: '#FFF' },
+          gridlines: {
+            interval: 0,
+          },
+        },
+        chartArea: {
+          left: 5,
+          top: 20,
+          width: '100%',
+          height: '350',
+        },
+        bar: { groupWidth: '100%' },
+        colors: ['#0366d6', '#FFF'],
+      };
+
+      const chart = new google.visualization.ComboChart(ldexChartRef.value);
+      chart.draw(data, options);
+    }
+  } catch (e) {}
 };
 
 watchEffect(() => props.chartData && calculateChart());
+
+onMounted(() => {
+  window.addEventListener('resize', calculateChart);
+});
+onUnmounted(() => {
+  window.removeEventListener('resize', calculateChart);
+});
 </script>
 
 <style lang="scss" scoped></style>
